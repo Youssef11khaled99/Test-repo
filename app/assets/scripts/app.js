ef7a9f99a7d13cae01f421b0c1e631658c7b7a5f
Youssef11khaled99 <uossef_khaled99@yahoo.com>
2018-07-27 18:14:17 +0200

    add and commit

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -29,3 +29,23 @@ function myMap() {
     });
     marker.setMap(map);
 }
+// nav-bar
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+// Responsive navbar
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
